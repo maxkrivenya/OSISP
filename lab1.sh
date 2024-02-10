5eddfdb3d2c195fdea728ef90d7b58ae6998a93a
Live System User <maxkrivenya@gmail.com>
2024-02-10 06:00:15 -0500

    stat does not return stuff

--- lab1.sh
+++ lab1.sh
@@ -3,11 +3,29 @@ optstring="ldf"
 curopt=""
 search_dir=./
 
+    #list
 #for entry in "$search_rit"/*
 #do
 #    echo "$entry"
 #done
-while getopts ${optstring} curopt
+
+    #echo getopt
+#while getopts ${optstring} curopt
+#do
+#    echo $curopt
+#done
+
+
+    #echo file permissions
+for entry in "$search_rit"/*
 do
+#    curopt= $((stat -c%A  $entry))
+    curopt = $(stat -c%A $entry)
     echo $curopt
+    firstlet=${curopt:0:1}
+    echo $firstlet
+#    if [ "$curopt" = "d" ]
+#    then
+#	echo "$entry"
+#    fi
 done
