1a59adb09bbcb724faae12b35ce1f6be497ba4a4
Maks Krivenya <117522968+maxkrivenya@users.noreply.github.com>
2024-02-11 12:08:51 -0500

    work in progress

--- lab1.cpp
+++ lab1.cpp
@@ -1,87 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
-#define PATHLENGTH 40
-#define SCRIPTNAME 24
-#define SCRIPT 80
-#define MASKLENGTH 5
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(int argc, char** argv){    
-    char mask[MASKLENGTH] = "";
-    char path[PATHLENGTH]= "";  
-    char scriptname[SCRIPTNAME] = "./lab1_script.sh";
-    size_t size_of_arg = 0;
-    size_t amt_of_masks = 0;
-    size_t i = 0;
-    size_t j = 0;
-    mask[MASKLENGTH-1]='\0';
-    for(i = 1; i < argc; i++){
-	size_of_arg = strlen(argv[i]);
-	if(argv[i][0]=='-'){
-	    for(j = 1; j < size_of_arg; j++){
-		switch(argv[i][j]){
-		    case 'f':{
-			mask[amt_of_masks]='-';
-			amt_of_masks++;
+void printEntry(struct dirent* ent, char type){
+	if(ent==NULL){return;}
+	int fildes = open(ent.name, 0_RDWR);
+	struct stat entstat;
+	int status = fstat(fildes, &entstat);
+	if(status==-1){
+		perror("fstat failed");
+		exit(1);
+	}
+	switch(type){
+		case 'f':{
+			if(S_ISREG(entstat.st_mode)){
+				printf("\nType:'%c'\tName:'%s'\n",'a'+(ent->d_type-1),ent->d_name);
+			}
 			break;
-		    }
-		    case 'd':{
-			mask[amt_of_masks]='d';
-			amt_of_masks++;
-			break;
-		    }
-		    case 'l':{
-			mask[amt_of_masks]='l';
-			amt_of_masks++;
-			break;
-		    }
-		    default:{
-			printf("Error argv value:'%s', %d , %d", argv[i], j, size_of_arg);
-			exit(1);
-		    }
 		}
-	    }
-	}else{
-	    for(j = 0; j < PATHLENGTH && j < size_of_arg; j++){
-	        path[j] = argv[i][j];
-	    }
-	    path[j] = '\0';
-        }
-    }
-    char script[SCRIPT] = "";
-    for(i = 0; i < SCRIPTNAME && scriptname[i] != '\0'; i++){
-	script[i] = scriptname[i];
-    } 
-    script[i]=' ';  
-    i++;
-//    printf("mask == '%s'\0", mask);
-    if(mask[0] == '\0'){
-	mask[0]='-';
-	mask[1]='l';
-	mask[2]='d';
-	mask[MASKLENGTH-1] = '\0';
-    }
-//    printf("mask == '%s'\0",mask);
-    for(j = 0; j < MASKLENGTH && mask[j] != '\0'; i++ && j++){
-	script[i] = mask[j];
-    }    
-    script[i]=' ';
-    i++;
-//    printf("path == '%s'\0",path);
-    if(path[0]=='\0'){
-	path[0]='~';
-	path[1]='\0';
-    }
-//    printf("path == '%s'\0",path);
-    for(j = 0; j < PATHLENGTH  && path[j] != '\0'; i++ && j++){
-//	printf("where is my fucking ~\0");
-	script[i] = path[j];
-//	printf("%s", script);
-    }    
-    i++;
-    script[i]='\0';
-    printf("%s", script);
-    
-    system(script);
-    return 0;
+		case 'd':{
+			if(S_ISDIR(entstat.st_mode)){
+				printf("\nType:'%c'\tName:'%s'\n",'a'+(ent->d_type-1),ent->d_name);
+			}
+			break;
+		}
+		case 'l':{
+			if(S_ISLNK(entstat.st_mode)){
+				printf("\nType:'%c'\tName:'%s'\n",'a'+(ent->d_type-1),ent->d_name);
+			}
+			break;
+		}
+		default:{
+			printf("\nweird printEntry type: '%c'", type);
+			break;
+		}
+	}
+	status = close(fildes);
+	if(status == -1){
+		perror("file not closed!!!");
+		exit(1);
+	}
+	return;
+}
+
+int main(int argc, char** argv){
+	DIR* dirp = NULL;
+	int flag = 0;
+	size_t i = 0;
+	size_t j = 0;
+	size_t curr_arg_len=0;
+	size_t mask_size = 0;
+	struct dirent* ent = NULL;
+	struct stat entstat;
+	bool fflag=true;
+	bool dflag=true;
+	bool lflag=true;
+
+	for(i=1; i < argc; i++){
+		if(argv[i][0] != ' '){
+			dirp = opendir(argv[i]);
+		}
+		if(dirp!=NULL){	
+			perror("DIR open error");
+			exit(1);
+		}	
+
+	for(i=1;i<argc;i++){	
+		curr_arg_len=strlen(argv[i]);
+		if(curr_arg_len<=1){
+			perror("short arg");
+			exit(1);
+		}
+		if(argv[i][0]=='-'){
+			for(j=1;j<curr_arg_len;j++){
+				switch(argv[i][j]) {
+					case 'f':{
+						if(fflag==true){
+							fflag=false;
+							do{
+								ent = readdir(dirp);
+								printEntry(ent, 'f');
+							}while(ent != NULL);
+							rewinddir(dirp);
+						}		
+					}
+				}
+			}
+		}
+
+	}
+
+	
+	flag = dirclose(dirp);
+	if(flag==-1){
+		perror("DIR failed to close!");
+		exit(1);
+	}
+	return 0;
 }
