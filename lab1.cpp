cf38b5c45ec5fd45c5ab174946f549c9cbbe7816
Maks Krivenya <117522968+maxkrivenya@users.noreply.github.com>
2024-02-11 16:52:33 -0500

    very close

--- lab1.cpp
+++ lab1.cpp
@@ -6,140 +6,96 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-void printEntry(struct dirent* ent, char type){			//function for printing a single dir entry
-	if(ent==NULL){
-		return;
-	}
-	struct stat entstat;
-	int status = lstat(ent->d_name, &entstat);	//get entry status to see its filetype
-	if(status==-1){
-		perror("fstat failed");
-		exit(1);
-	}
-	switch(type){		//depending on what parameter is passed
-		case 'f':{
-			if(S_ISREG(entstat.st_mode)){			//if file, print
-				printf("\nFIL\t'%s'\n",ent->d_name); 
-			}
-			break;
-		}
-		case 'd':{
-			if(S_ISDIR(entstat.st_mode)){			//if dir, print
-				printf("\nDIR\t'%s'\n",ent->d_name);
-			}
-			break;
-		}
-		case 'l':{
-			if(S_ISLNK(entstat.st_mode)){			//if lnk, print
-				printf("\nLNK\t'%s'\n",ent->d_name);
-			}
-			break;
-		}
-		default:{
-			printf("\nweird printEntry type: '%c'", type);
-			break;
-		}
-	}
-	return;
-}
+#define OPTMASK "fdls"
+#define DEFAULT_DIR "./"
 
 int main(int argc, char** argv){
 	
 	//error checker
 	int flag = 0;
 	
-	//iterators
-	size_t i = 0; 
-	size_t j = 0;
-
-	//argv[i] size holder
-	size_t curr_arg_len=0;
-	
 	//sys vars
 	DIR* dirp = NULL;
 	struct dirent* ent = NULL;
 	struct stat entstat;
 	
-	//flags for optparameters
-	bool fflag=true;
-	bool dflag=true;
-	bool lflag=true;
+	//getopt holder
+	char opt;
 	
-	//find dir path in parameters [1/-1]
-	for(i=1; i < argc; i++){
-		if(argv[i][0] != '-' && dirp==NULL){
-			dirp = opendir(argv[i]);
-		}
-
-	}
-	if(dirp==NULL){
-		dirp=opendir("./");	  //if invalid/none passed, open current
-	}
-	if(dirp==NULL){
-		perror("dir open error"); //error check
-		exit(1);
-	}
-
-	for(i=1;i<argc;i++){				//looking for opt pars in argv
-		curr_arg_len=strlen(argv[i]);
-		if(curr_arg_len<=1){
-			perror("short arg");
-			exit(1);
-		}
-		if(argv[i][0]=='-'){			//if opt par
-			for(j=1;j<curr_arg_len;j++){	//see what it includes
-				switch(argv[i][j]) {	//for each data type: switch flag to false, for each file in dir: call print with current type
-					case 'f':{
-						if(fflag==true){
-							fflag=false;
-							do{
-								ent = readdir(dirp);
-								printEntry(ent, 'f');
-							}while(ent != NULL);
-							rewinddir(dirp);
-						}
-						break;
-					}
-					case 'd':{
-						if(dflag==true){
-							dflag=false;
-							do{
-								ent = readdir(dirp);
-								printEntry(ent, 'd');
-							}while(ent != NULL);
-							rewinddir(dirp);
-						}
-						break;
-					}
-					case 'l':{
-						if(lflag==true){
-							lflag=false;
-							do{
-								ent = readdir(dirp);
-								printEntry(ent, 'l');
-							}while(ent != NULL);
-							rewinddir(dirp);
-						}
-						break;
-					}
-								 
-					default:{
-						printf("\nNaF - '%c'\n", argv[i][j]);
-						break;
-					}
-				}
+	//flags for optparameters
+	bool fflag=false;
+	bool dflag=false;
+	bool lflag=false;
+	bool sflag=false;	
+	do{
+		opt = getopt(argc,argv, OPTMASK);
+		if(opt!=-1){
+		switch(opt){
+			case 'f':{
+				fflag=true;
+				break;
+			}
+			case 'd':{
+				dflag=true;
+				break;
+			}
+			case 'l':{
+				lflag=true;
+				break;
+			}
+			case 's':{
+				sflag=true;
+				break;
+			}
+			default:{
+				perror("unknown opt param");
+				exit(1);
+				//printf("\nunknown opt param:'%c'\n",opt);
+				//break;
 			}
 		}
+		}
+	}while(opt!=-1);
 
+	if(optind<argc){
+		dirp=opendir(argv[optind]);
 	}
-
+	if(dirp==NULL){
+		dirp=opendir(DEFAULT_DIR);
+	}
+	if(dirp==NULL){
+		perror("DIR open error");
+		exit(1);
+	}
 	
-	flag = closedir(dirp);			//close folder
+	do{
+		ent = readdir(dirp);
+		if(ent != NULL){
+			flag=lstat(ent->d_name,&entstat);
+			if(flag == -1){
+				perror("lstat failed");
+				exit(1);
+			}
+			if(S_ISREG(entstat.st_mode) && fflag){
+				printf("FIL\t%s\n",ent->d_name);
+				continue;
+			}
+			if(S_ISDIR(entstat.st_mode) && dflag){
+				printf("DIR\t%s\n",ent->d_name);
+				continue;
+			}
+			if(S_ISLNK(entstat.st_mode) && lflag){
+				printf("LNK\t%s\n",ent->d_name);
+				continue;
+			}
+		}
+	}while(ent != NULL);
+
+	flag = closedir(dirp);
 	if(flag==-1){
 		perror("DIR failed to close!");
 		exit(1);
 	}
-
+	
 	return 0;
 }
